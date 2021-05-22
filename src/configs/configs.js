d74992947e21e20c78495f375df43c1f13e27e4e
Bilal-Bangash <muhammadbilalbangash@hotmail.com>
2021-05-22 19:14:14 +0500

    remove configs

--- src/configs/configs.js
+++ src/configs/configs.js
@@ -1,10 +1,3 @@
 export const configs = {
   //configs
-  apiKey: "AIzaSyAIwHwmEa1pNAYZlacX25tOjADKR91moAA",
-  authDomain: "chat-app-b12d4.firebaseapp.com",
-  projectId: "chat-app-b12d4",
-  storageBucket: "chat-app-b12d4.appspot.com",
-  messagingSenderId: "843213801224",
-  appId: "1:843213801224:web:365945330d99165dfecebd",
-  measurementId: "G-YLY5X7VD45",
 };
